d4eacece1874f4d0bac557bb9a790a4347573476
Yuriy Susuk <yuriy.susuk@scout24.com>
2018-02-08 15:20:56 +0100

    experiments

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ use_plugin("python.unittest")
 use_plugin("python.coverage")
 use_plugin("python.install_dependencies")
 use_plugin("python.distutils")
+use_plugin("python.mypy")
 
 default_task = "publish"
 
@@ -14,6 +15,7 @@ def initialize(project):
     project.build_depends_on('pandas')
     project.build_depends_on('numpy')
     project.build_depends_on('matplotlib')
+    project.build_depends_on('mypy')
     # project.build_depends_on('json')
     # project.set_property('coverage_threshold_warn', 50)
     project.set_property('coverage_break_build', False)
